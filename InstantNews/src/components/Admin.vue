cec50cdfd2d4071fab92389e1e7300ee14ef4fae
Lissa Goforth <lissa.goforth@bti.tv>
2018-07-12 18:43:35 -0500

    debugging compiler errors

---
+++ InstantNews/src/components/Admin.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="admin">
+        <ol></ol>
+        <table>{{ users }}</table>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Admin",
+  data() {
+    return {
+      users: [
+        {
+          Username: "Test1",
+          FName: "Testfn",
+          LName: "Testln",
+          Email: "test1@test.com"
+        },
+        {
+          Username: "Test2",
+          FName: "Testfn",
+          LName: "Testln",
+          Email: "test2@test.com"
+        },
+        {
+          Username: "Test3",
+          FName: "Testfn",
+          LName: "Testln",
+          Email: "tes3t@test.com"
+        }
+      ]
+    };
+  }
+};
+</script>
--- InstantNews/src/router/index.js
+++ InstantNews/src/router/index.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@components/Login'
+import Register from '@components/Register'
+import ResetPw from '@components/ResetPw'
+import Admin from '@components/Admin'
+import VNews from '@components/VNews'
+import VWeather from '@components/VWeather'
 
 Vue.use(Router)
 
@@ -11,6 +16,36 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/resetpw',
+      name: 'ResetPw',
+      component: ResetPw
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Admin
+    },
+    {
+      path: '/vnews',
+      name: 'VNews',
+      component: VNews
+    },
+    {
+      path: '/vweather',
+      name: 'VWeather',
+      component: VWeather
     }
   ]
 })
