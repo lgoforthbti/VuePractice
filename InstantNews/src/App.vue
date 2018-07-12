a9ca69e631d8574e10f810e9329aa972db96ba80
Lissa Goforth <lissa.goforth@bti.tv>
2018-07-12 17:57:16 -0500

    Added routing, added links to test routing

--- InstantNews/src/App.vue
+++ InstantNews/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <router-link :to="{ name: 'HelloWorld'}">Home</router-link>
+    <router-link :to="{ name: 'Login'}">Login</router-link>
+    <router-link :to="{ name: 'Register'}">Register</router-link>
+    <router-link :to="{ name: 'Admin'}">Admin</router-link>
+    <router-link :to="{ name: 'VNews'}">News</router-link>
+    <router-link :to="{ name: 'VWeather'}">Weather</router-link>
     <router-view/>
   </div>
 </template>
@@ -10,14 +15,3 @@ export default {
   name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
