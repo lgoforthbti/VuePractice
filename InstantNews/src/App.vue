80dea1acff70a643c31719f0d0ac7bb978f71ecf
Lissa Goforth <lissa.goforth@bti.tv>
2018-07-12 19:20:52 -0500

    still debugging compiler

--- InstantNews/src/App.vue
+++ InstantNews/src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
     <router-link :to="{ name: 'HelloWorld'}">Home</router-link>
-    <router-link :to="{ name: 'Login'}">Login</router-link>
-    <router-link :to="{ name: 'Register'}">Register</router-link>
-    <router-link :to="{ name: 'Admin'}">Admin</router-link>
-    <router-link :to="{ name: 'VNews'}">News</router-link>
-    <router-link :to="{ name: 'VWeather'}">Weather</router-link>
+    <router-link to="/login">Login</router-link>
+    <router-link to="/register">Register</router-link>
+    <router-link to="/resetpw">Reset Password</router-link>
+    <router-link to="/admin">Admin</router-link>
+    <router-link to="/vnews">News</router-link>
+    <router-link to="/vweather">Weather</router-link>
     <router-view/>
   </div>
 </template>
--- InstantNews/src/components/Admin.vue
+++ InstantNews/src/components/Admin.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="admin">
-        <ol></ol>
+        <ol v-for="u in users" v-bind:key="u.Id">
+            <li>{{u.Username, u.FName, u.LName, u.Email}}</li>
+        </ol>
         <table>{{ users }}</table>
     </div>
 </template>
@@ -12,18 +14,21 @@ export default {
     return {
       users: [
         {
+          Id: 1,      
           Username: "Test1",
           FName: "Testfn",
           LName: "Testln",
           Email: "test1@test.com"
         },
         {
+          Id: 2,
           Username: "Test2",
           FName: "Testfn",
           LName: "Testln",
           Email: "test2@test.com"
         },
         {
+          Id: 3,
           Username: "Test3",
           FName: "Testfn",
           LName: "Testln",
